caaf979836402dfb6e14bc9d15343eac6c12aa07
farouk <faroukbr050@gmail.com>
2021-01-29 20:46:02 +0100

    Added build directory

---
+++ client/build/precache-manifest.fa52bdecc9d6d7b1ba4dde1f8bb0140b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe2446169c1d9966acd120871d043ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76225bc022f27c59b95e",
+    "url": "/static/css/2.55e5954d.chunk.css"
+  },
+  {
+    "revision": "c6521af37f746a754bb5",
+    "url": "/static/css/main.9712c18f.chunk.css"
+  },
+  {
+    "revision": "76225bc022f27c59b95e",
+    "url": "/static/js/2.9c0ae297.chunk.js"
+  },
+  {
+    "revision": "5bf9dfed698d4ec2c7ce5219feb231cf",
+    "url": "/static/js/2.9c0ae297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6521af37f746a754bb5",
+    "url": "/static/js/main.7e59c4b3.chunk.js"
+  },
+  {
+    "revision": "bdf121516d7653dcf912",
+    "url": "/static/js/runtime-main.e04325e3.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  }
+]);
